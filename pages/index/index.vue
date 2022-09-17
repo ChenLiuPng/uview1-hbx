2287ed2308d065c12b893706337bb9277632bc29
chenliupeng <vclp@163.com>
2022-09-18 06:55:17 +0800

    feat:init

---
+++ pages/index/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="content">
+		<u-button >默认按钮</u-button>
+		<u-button type="primary">主要按钮</u-button>
+		<u-button type="success">成功按钮</u-button>
+		<u-button type="info">信息按钮</u-button>
+		<u-button type="warning">警告按钮</u-button>
+		<u-button type="error">危险按钮</u-button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		padding: 20rpx;
+		uni-button{
+			margin-bottom: 20rpx;
+		}
+	}
+</style>
